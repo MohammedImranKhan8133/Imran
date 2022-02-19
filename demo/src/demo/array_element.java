17f46e5ed3db4f6d35f3cecd400e1ff112e812e4
Imran <Imran@DESKTOP-QPD5K93>
2022-02-19 17:44:25 +0530

    commit

---
+++ demo/src/demo/array_element.java
@@ -0,0 +1,42 @@
+package demo;
+
+import java.util.Scanner;
+class demo{
+	public void prime()
+	{
+		int no=3;
+	       while(no<20)
+	       {
+	       boolean prime=true;
+	       for(int i=2;i<no;i++)
+	       {
+	    	   int s=no%i;
+	    	   if(s==0)
+	    	   {
+	    		   prime=false;
+	    	   }
+	       }
+	    	   if(prime)
+	    	   {
+	            System.out.println(no+" "+"is prime");
+	    	   }
+	    	   no=no+2;
+	       }
+	    	   
+	}
+	public void array()
+	{
+	}
+}
+public class array_element {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      String ar[]= {"imran","sri","thameem"};
+      for(String p:ar)
+      {
+    	  System.out.println(p);
+      }
+	}
+
+}
---
+++ demo/src/demo/helloworld.java
@@ -0,0 +1,160 @@
+package demo;
+import java.util.Scanner;
+ class problem{
+
+	public void fibonnaci()
+	 {
+		int a=0;//1
+		int b=1;//0
+		int c=0;//1
+		 for(int d=1;d<=8;d++)
+		  {
+			System.out.println(a);//0
+			a=b+c;
+			b=c;
+			c=a;
+		  }
+	}
+	public void factorial()
+	{
+		int a;
+		int b=1;
+		for(a=1;a<=5;a++)//5!=5*4*3*2*1
+		{
+	     b=b*a;
+		}
+		System.out.println(b);
+	}
+	public void prime()
+	{
+		int no=20;
+		 for(int a=1;a<=no;a++)
+		 {
+			if(a%2==0)
+			{
+	          System.out.println("no"+ " "+a+" is not prime");
+			}
+			else
+	System.out.println(a+" "+"is prime");
+		}
+		
+	}
+	public void palindrom()
+	{ 
+		int no=121;
+		int d=no;
+		int r=0;
+		while(no>0)
+		{
+		r=(r*10)+no%10;//4
+		no=no/10;
+		
+		}
+		if(d==r)
+	System.out.println(d+ "  is palindrom");
+		else
+			System.out.println(d+" is not panlidrom");
+		
+	}
+  }
+ class encapulation
+ {
+	 private int salary;
+	 public void get()
+	 {
+		 System.out.print(salary);
+	 }
+	 public void set(int s)
+	 {
+		 this.salary=s;
+	 }
+ }
+ enum day{
+     MONDAY,
+	 TUESDAY,
+	 WEDNESDAY,
+	 THURSDAY,
+	 FRIDAY;
+ }
+ interface animal
+ {
+	 public  void tiger();
+ }
+ class a implements animal{
+	 public void tiger()
+	  {
+		  System.out.println("tiger");
+	  }
+ }
+ class pr{
+	 protected int age=20;
+ }
+ class vowl
+ {
+	public void check()
+	{
+		
+	}
+ }
+public class helloworld {
+  public static void main(String args[])
+  {
+	 /* System.out.println("hello imran");
+	problem obj=new problem();
+	 obj.palindrom();
+	 Scanner in=new Scanner(System.in);
+	 System.out.println("choose the correct answer ");
+	  System.out.println("which is global language:");
+	  System.out.println("(a)english (b)german (c)spansih");
+	  String input=in.nextLine();
+	  
+	 switch(input)
+	   {
+	   case "a":
+		   System.out.println("correct");
+		   break;
+	   case "b":
+		   System.out.println("wrong");
+		   break;
+	   case "c":
+		   System.out.println("wrong");
+		   break;
+		   default:System.out.println("correct answer is a");
+	   }
+	 encapulation en=new encapulation();
+	 en.set(100000);
+	 en.get();
+	 day d=day.MONDAY;
+	 System.out.println("\n"+d);
+	 animal a=new a();
+	 a.tiger();
+	 helloworld h=new helloworld();
+	 pr p=new pr();
+	 int b=p.age;
+	System.out.println(b);
+	Scanner s=new Scanner(System.in);
+	  String find=s.nextLine();
+	  String ch=find;
+if(ch=="a"||ch=="e"||ch=="i"||ch=="o"||ch=="u")
+	  {
+		 System.out.println(find+"\t"+"its vowl");
+	  }
+	  else
+	  {
+		  System.out.println(find+" "+"no its not vowl");
+	  }*/
+	  Scanner s=new Scanner(System.in);
+	  System.out.println("enter your alphabte");
+	  String ch=s.nextLine();
+if(ch=="ae"||ch=="e"||ch=="i"||ch=="o"||ch=="u")
+	  {
+		  System.out.println("vowl");
+	  }
+	  else
+	  {
+		  System.out.println("not vowl");
+	  }
+	  
+	  
+  }
+}
